fce954a86591ec18982cfe56b46166517938c69d
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-24 16:14:55 +0530

    [Ravina] Add: Checking if FirstName is valid or not

---
+++ UserRegistration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DAY_19_UserRegistration
+{
+    public class UserRegistration
+    {
+        public static string Regex_FirstName = "^[A-Z][a-z]{2,}$";
+        public static bool ValidateFirstName(string FirstName)
+        {
+
+
+            return Regex.IsMatch(FirstName, Regex_FirstName);
+
+        }
+
+        public static void CheckFirstName(string FirstName)
+        {
+            if (Regex.IsMatch(FirstName, Regex_FirstName) == false)
+            {
+                Console.WriteLine("Enter valid Name in proper format ");
+                
+            }
+            else
+            {
+                Console.WriteLine("is valid ");
+            }
+
+        }
+
+    }
+}
