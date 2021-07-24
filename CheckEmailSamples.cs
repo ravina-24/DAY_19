52f61b466f1e1497b7882094be3bf52f7eac2e76
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-24 23:05:55 +0530

    [Ravina] Add: Checking given sample emails

---
+++ CheckEmailSamples.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DAY_19_UserRegistration
+{
+   public  class CheckEmailSamples
+    {
+        
+        
+            public static string Regex_SampleEmails = "^[A-Za-z]+([-.+#^*][0-9]+)?[0-9]*[@][A-Za-z0-9]+[.][A-Za-z]{2,}([.][a-zA-Z]{2,})?$";
+
+            public static bool ValidateEmail(string SampleEmail)
+            {
+                return Regex.IsMatch(SampleEmail, Regex_SampleEmails);
+            }
+        }
+    }
+
