084c1df50dc9454b57db3f048b53692f94970e24
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-24 16:30:22 +0530

    [Ravina] Add: Checking last name

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -8,6 +8,7 @@ namespace DAY_19_UserRegistration
     public class UserRegistration
     {
         public static string Regex_FirstName = "^[A-Z][a-z]{2,}$";
+        public static string Regex_LastName = "^[A-Z][a-z]{2,}$";
         public static bool ValidateFirstName(string FirstName)
         {
 
@@ -23,12 +24,28 @@ namespace DAY_19_UserRegistration
                 Console.WriteLine("Enter valid Name in proper format ");
                 
             }
-            else
-            {
-                Console.WriteLine("is valid ");
-            }
+            
 
         }
+
+        public static bool ValidateLastName(string LastName)
+        {
+
+
+            return Regex.IsMatch(LastName, Regex_LastName);
+
+        }
+        public static void CheckLastName(string LastName)
+        {
+            if (Regex.IsMatch(LastName, Regex_LastName) == false)
+            {
+                Console.WriteLine("Enter valid LastName in proper format ");
+
+            }
+           
+
+        }
+
 
     }
 }
