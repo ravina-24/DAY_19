e8fd3566b64308f417627fcb5972a29746d195ea
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-24 21:42:01 +0530

    [Ravina] Add: Checking password if it contains atleart 8 characters and one upper case character and number and special character

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -12,7 +12,7 @@ namespace DAY_19_UserRegistration
         public static string Regex_EmailId= "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
         public static string Regex_PhoneNumber = "^[0-9]{2}[ ][6-9][0-9]{9}$";
 
-        public static string Regex_Password = "^((?=.*[A-Z])(?=.*[0-9])(?=.{8,}))";
+        public static string Regex_Password = "^((?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_.])(?=.{8,}))";
 
 
         public static bool ValidateFirstName(string FirstName)
