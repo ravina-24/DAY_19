20836a99a1586b7617e25c6c2a946e3baeeb988a
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-24 16:11:03 +0530

    [Ravina] Add: Main Program File

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DAY_19_UserRegistration
+{
+   public  class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration ");
+
+            string FirstName;
+            Console.Write("Enter First Name : ");
+            FirstName = Console.ReadLine();
+
+            UserRegistration.CheckFirstName(FirstName);
+            Console.ReadKey();
+
+        }
+    }
+}
